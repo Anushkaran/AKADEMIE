f9006b1fa54909c9bfdf75ad49f9d25c093549fe
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-06-10 01:00:02 +0100

    update User Controller and center Controller

--- app/Http/Controllers/Web/Admin/CenterController.php
+++ app/Http/Controllers/Web/Admin/CenterController.php
@@ -2,10 +2,82 @@
 
 namespace App\Http\Controllers\Web\Admin;
 
+use App\Contracts\CenterContract;
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CenterController extends Controller
 {
-    //
+    protected $center;
+
+    public function __construct(CenterContract $center)
+    {
+        $this->center = $center;
+    }
+
+    public function index()
+    {
+        $centers = $this->center->findByFilter();
+        return view('admin.centers.index',compact('centers'));
+    }
+
+    public function create()
+    {
+        return view('admin.centers.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $this->center->new($this->getValidatedData($request));
+        session()->flash('success','messages.create');
+        return redirect()->route('admin.centers.index');
+    }
+
+    public function show($id) : Renderable
+    {
+        $center = $this->center->findOneById($id);
+        return view('admin.centers.show',compact('center'));
+    }
+
+
+    public function edit($id) : Renderable
+    {
+        $center = $this->center->findOneById($id);
+        return view('admin.centers.edit',compact('center'));
+    }
+
+    /**
+     * @param $id
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function update($id,Request $request): RedirectResponse
+    {
+        $this->center->update($id,$this->getValidatedData($request));
+        session()->flash('success','messages.update');
+        return redirect()->route('admin.centers.index');
+    }
+
+    /**
+     * @param $id
+     * @return RedirectResponse
+     */
+    public function destroy($id): RedirectResponse
+    {
+        $this->center->delete($id);
+        session()->flash('success','messages.delete');
+        return redirect()->route('admin.centers.index');
+    }
+
+    private function getValidatedData(Request $request): array
+    {
+        return $request->validate([
+            'name' => 'required|string|max:100',
+            'description' => 'sometimes|nullable|string|max:250',
+            'phone' => 'required|string|max:15',
+            'address' => 'required|string|max:200',
+        ]);
+    }
 }
--- app/Http/Controllers/Web/Admin/UserController.php
+++ app/Http/Controllers/Web/Admin/UserController.php
@@ -2,10 +2,91 @@
 
 namespace App\Http\Controllers\Web\Admin;
 
+use App\Contracts\UserContract;
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    protected $user;
+
+    public function __construct(UserContract $user)
+    {
+        $this->user = $user;
+    }
+
+    public function index()
+    {
+        $users = $this->user->findByFilter();
+        return view('admin.users.index',compact('users'));
+    }
+
+    /**
+     * @return Renderable
+     */
+    public function create() : Renderable
+    {
+        return view('admin.users.create');
+    }
+
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $this->user->new($this->getValidatedData($request));
+        session()->flash('success',__('messages.update'));
+        return redirect()->route('admin.users.index');
+    }
+
+    /**
+     * @param $id
+     * @return Renderable
+     */
+    public function show($id) : Renderable
+    {
+        $user = $this->user->findOneById($id);
+        return view('admin.users.show',compact('user'));
+    }
+
+    /**
+     * @param $id
+     * @return Renderable
+     */
+    public function edit($id) : Renderable
+    {
+        $user = $this->user->findOneById($id);
+        return view('admin.users.edit',compact('user'));
+    }
+
+    /**
+     * @param $id
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function update($id,Request $request): RedirectResponse
+    {
+        $this->user->update($id,$this->getValidatedData($request));
+        session()->flash('success',__('messages.update'));
+        return redirect()->route('admin.users.index');
+    }
+
+    /**
+     * @param $id
+     * @return RedirectResponse
+     */
+    public function destroy($id)
+    {
+        $this->user->delete($id);
+        session()->flash('success',__('messages.delete'));
+        return redirect()->route('admin.users.index');
+    }
+
+    private function getValidatedData(Request $request): array
+    {
+        return $request->validate([]);
+    }
 }
