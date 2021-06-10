6196ce7413f7631a6c22a5ad0af23925cb7f82f6
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-06-10 01:22:25 +0100

    create lang.php

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'create' => 'Record has been created successfully',
+    'update' => 'Record has been Updated successfully',
+    'delete' => 'Record has been deleted successfully',
+    'fail' => 'Oops! Something went worn, please try again.',
+];
