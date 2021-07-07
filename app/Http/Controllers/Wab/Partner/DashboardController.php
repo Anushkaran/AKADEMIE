af1c3a4becd6873cd516cdb7ca10416aaacc85d8
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-07-07 18:49:05 +0100

    Create Partner Authentication Views, route/partner.php and Partner dashboard controller

---
+++ app/Http/Controllers/Wab/Partner/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Wab\Partner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //
+}
---
+++ routes/partner.php
@@ -0,0 +1,23 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+
+Route::redirect('partner','admin/dashboard');
+
+Route::get('login',[\App\Http\Controllers\Web\Partner\Auth\PartnerLoginController::class,'index'])->name('login.index');
+Route::post('login',[\App\Http\Controllers\Web\Partner\Auth\PartnerLoginController::class,'login'])->name('login');
+
+Route::get('password/reset/{token}',[\App\Http\Controllers\Web\Partner\Auth\ResetPasswordController::class,'showResetForm'])->name('password.reset');
+Route::post('password/reset',[\App\Http\Controllers\Web\Partner\Auth\ResetPasswordController::class,'reset'])->name('reset');
+
+Route::get('forgot/password',[\App\Http\Controllers\Web\Partner\Auth\ForgotPasswordController::class,'showLinkRequestForm'])->name('forgot.password.email');
+Route::post('forgot/password',[\App\Http\Controllers\Web\Partner\Auth\ForgotPasswordController::class,'sendResetLinkEmail'])->name('forgot.password.send');
+
+
+Route::middleware('auth:admin')->group(function (){
+    Route::any('logout',[\App\Http\Controllers\Web\Partner\Auth\PartnerLoginController::class,'logout'])->name('logout');
+
+    Route::get('dashboard',[App\Http\Controllers\Web\Partner\DashboardController::class,'index'])->name('dashboard');
+
+});
