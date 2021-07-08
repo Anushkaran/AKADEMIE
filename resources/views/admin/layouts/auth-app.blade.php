d09fdc43239446bf674e34363ae5179c9fbd374b
chouaib <chou2597@gmail.com>
2021-07-08 16:09:41 +0100

    CHANGEMENT DE DESCRIPTION ET AUTHERU HEADER

--- resources/views/admin/layouts/auth-app.blade.php
+++ resources/views/admin/layouts/auth-app.blade.php
@@ -6,9 +6,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="PIXINVENT">
+    <meta name="description" content=" une application permettant de gérer les formations dispensées aux collaborateurs et de piloter les compétences et les ressources .">
+    <meta name="keywords" content=" une application permettant de gérer les formations dispensées aux collaborateurs et de piloter les compétences et les ressources .">
+    <meta name="author" content="DEVELOP/IT">
     <title>@yield('title') - {{config('app.name')}}</title>
     <link rel="apple-touch-icon" href="{{asset('assets/vuexy/app-assets/images/ico/apple-icon-120.png')}}">
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/vuexy/app-assets/images/ico/favicon.ico')}}">
