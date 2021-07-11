cfab7c576966ca9a2bce68b5d4d9071c2be4e840
chouaib <chou2597@gmail.com>
2021-07-11 15:23:23 +0100

    translation fils to fr END

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Votre mot de passe a été modifié!',
+    'sent' => 'Un email avec un lien d récuperation a été envoyé a votre adresse!',
+    'throttled' => 'Veuillez attendre avant de réessayer.',
+    'token' => "ce lien de recuperation n'est pas valide.",
+    'user' => "Aucun utilisateur existant avec cette adresse.",
 
 ];
