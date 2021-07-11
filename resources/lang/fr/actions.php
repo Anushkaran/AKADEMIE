94bfd30330b3d84e60eff613c55477bc30bce9cb
chouaib <chou2597@gmail.com>
2021-07-11 13:13:24 +0100

    traduction française

---
+++ resources/lang/fr/actions.php
@@ -0,0 +1,19 @@
+<?php
+
+
+return [
+    'create' => 'Creer',
+    'save' => 'Sauvgarder',
+    'update' => 'Enregistrer',
+    'edit' => 'Modifier',
+    'edit-password' => 'Changer mon mot de passe',
+    'delete' => 'Supprimer',
+    'add' => 'Ajouter',
+    'add-new' => 'Ajouter Nouveau :name',
+    'cancel' => 'Annuler',
+    'details' => 'Details',
+    'delete_confirm_title' => 'êtes vous sûr ?',
+    'delete_confirm_text' => "Vous ne pouvez pas modifier ça!",
+    'delete_btn_yes' => 'Oui',
+    'delete_btn_cancel' => 'Annuler',
+];
---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'create' => 'Record has been created successfully',
+    'update' => 'Record has been Updated successfully',
+    'delete' => 'Record has been deleted successfully',
+    'fail' => 'Oops! Something went worn, please try again.',
+    'welcome_to' => 'Welcome to :Name!  👋',
+    'welcome' => 'Welcome.',
+    'login_message' => 'Please sign-in to your account and start your session.',
+    'forgot_message' => 'Enter your email and we\'ll send you instructions to reset your password.',
+    'reset_message' => 'Your new password must be different from previously used passwords.',
+    'forgot_password' => 'Forgot Password?',
+    'reset_password_text' => 'Reset your password from here 🔒',
+    'reset_password' => 'Reset Password 🔒',
+];
+
