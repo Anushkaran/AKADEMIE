8238a28000bd1754cce83eb724505e813f25854e
chouaib <chou2597@gmail.com>
2021-08-04 17:21:27 +0100

    KEYWORDS AND PAGE DESCRIPTION

--- resources/views/partner/layouts/auth-app.blade.php
+++ resources/views/partner/layouts/auth-app.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description" content="bienvenue sur l'espace partenaire d'akademie.">
+    <meta name="keywords" content="akademie">
     <meta name="author" content="PIXINVENT">
     <title>@yield('title') - {{config('app.name')}}</title>
     <link rel="apple-touch-icon" href="{{asset('assets/vuexy/app-assets/images/ico/apple-icon-120.png')}}">
