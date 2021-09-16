3d7e14f4fb0cd7e7f9264cda5028870050496e8c
KaraAliAbdElbasset <karaliabdelbasset@outlook.com>
2021-09-16 22:04:22 +0100

    student details

--- resources/views/admin/evaluations/student.blade.php
+++ resources/views/admin/evaluations/student.blade.php
@@ -165,7 +165,7 @@
                                                                                 @if($session->tasks->contains($t->id))
                                                                                     <span class="badge badge-pill badge-light-success mr-1">validées</span>
                                                                                 @else
-                                                                                    <span class="badge badge-pill badge-light-success mr-1">non validées</span>
+                                                                                    <span class="badge badge-pill badge-light-danger mr-1">non validées</span>
                                                                                 @endif
                                                                             </td>
 
