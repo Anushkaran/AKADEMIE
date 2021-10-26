f043b86bf7275e8459f2614afbb2c0330f7cdae0
KaraAliAbdElbasset <karaliabdelbasset@outlook.com>
2021-10-26 20:17:20 +0100

    update routes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,7 +42,7 @@ class Handler extends ExceptionHandler
                 return response()->json([
                     'success' => false,
                     'message' => 'Object not found'
-                ]);
+                ],404);
             }
         });
     }
