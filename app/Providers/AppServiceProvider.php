431c77ffbe46307b05b0d1a0cebb8548d6da45fd
KaraAliAbdElbasset <karaliabdelbasset@outlook.com>
2021-10-26 17:47:10 +0100

    set length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,8 +23,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
+        Schema::defaultStringLength(191);
         Paginator::useBootstrap();
     }
 }
