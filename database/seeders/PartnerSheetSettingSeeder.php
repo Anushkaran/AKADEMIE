a7b237ec7b3ba824e140aafc753498e1971dacd5
KaraAliAbdElbasset <karaliabdelbasset@outlook.com>
2022-02-14 14:55:20 +0100

    Partner Sheet setting

---
+++ database/seeders/PartnerSheetSettingSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PartnerSheetSettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
